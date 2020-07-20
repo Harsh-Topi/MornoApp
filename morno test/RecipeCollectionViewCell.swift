ad071d33d8cf20a6a8fc027f02429d6c35e1785c
Harsh Topiwala <harshtopiwala@Harshs-Mac.local>
2020-07-20 11:48:41 -0700

    made changes to storyboard

---
+++ morno test/RecipeCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeCollectionViewCell.swift
+//  morno test
+//
+//  Created by Harsh Topiwala on 2020-07-20.
+//  Copyright © 2020 Harsh Topiwala. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var featuredImageView: UIImageView!
+    @IBOutlet weak var backgroundColorView: UIView!
+    @IBOutlet weak var interestTitle: UILabel!
+}
